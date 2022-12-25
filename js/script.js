94bb210c6bced1dfd453456e20cba70dae262026
YuriSoma <106472235+YuriSoma@users.noreply.github.com>
2022-12-25 15:20:38 +0300

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function memberCreation (){
+	var memberBuilder =
+		var count = yaser;
+ 		"<div class='col-auto g-0' style='background-color: lightgoldenrodyellow;'><div id='member'>${{count}}</div>";
+		document.getElementById("members-land").innerHTML = memberBuilder;
+		/*memberBuilder = "<div class='col-auto g-0' style='background-color: lightgoldenrodyellow;'><div id='member'>2</div>";
+		document.getElementById("members-land").innerHTML = memberBuilder; */
+}
+
+memberCreation();
