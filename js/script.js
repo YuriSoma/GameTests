e83394652592e17764017302f9dd70469f18198c
YuriSoma <106472235+YuriSoma@users.noreply.github.com>
2022-12-26 15:21:20 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
-function memberCreation (){
-	var memberBuilder =
-		var count = yaser;
- 		"<div class='col-auto g-0' style='background-color: lightgoldenrodyellow;'><div id='member'>${{count}}</div>";
-		document.getElementById("members-land").innerHTML = memberBuilder;
-		/*memberBuilder = "<div class='col-auto g-0' style='background-color: lightgoldenrodyellow;'><div id='member'>2</div>";
-		document.getElementById("members-land").innerHTML = memberBuilder; */
+var addNum = 1;
+var createDiv = "<div class='col-auto g-0''><div id='member'>"+addNum+"</div></div>";
+var sumCreate = "";
+for (var i=0; i < 50;i++ ) {
+	sumCreate += createDiv;
+	document.getElementById("members-land").innerHTML = sumCreate;
 }
-
-memberCreation();
