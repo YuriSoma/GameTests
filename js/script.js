8cf90cd2f5de094758188b950467cc1f4e059d97
YuriSoma <106472235+YuriSoma@users.noreply.github.com>
2022-12-27 15:56:35 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
-var addNum = 0;
-var sumCreate = "";
-function callCreate(addNum) {
-	var createDiv = "<div class='col-auto g-0''><div id='member'>"+addNum+"</div></div>";
-	sumCreate += createDiv;
-	return document.getElementById("members-land").innerHTML = sumCreate;
+function bulidGameLand (){
+	/*var addNum = 0;
+	var sumCreate = "";
+	function callCreate(addNum) {
+		var createDiv = "<div class='col-auto g-0'><div id='member'><button>"+addNum+"</button></div></div>";
+		sumCreate += createDiv;
+		return document.getElementById("members-land").innerHTML = sumCreate;
+	}
+
+	for (var i=0; i < 504;i++ ) {
+		counter = i + 1;
+		callCreate(counter);
+	} */
+
 }
 
-for (var i=0; i < 500;i++ ) {
-	counter = i + 1;
-	callCreate(counter);
+function playWindow() {
+	let playBoard;
+	playBoard = window.open("playBoard.html", "", "width=400,height=900")
 }
