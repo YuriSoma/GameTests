c23613f9d47cf7a36f0b7c6b156692441a356b66
YuriSoma <yaser.alelayan@outlook.com>
2023-01-05 14:26:44 +0300

    Refresh Uploading

--- js/script.js
+++ js/script.js
@@ -2,33 +2,57 @@ function bulidGameLand (){
 	var addNum = 0;
 	var sumCreate = "";
 	function callCreate(addNum) {
-		/* var createDiv = "<div class='col-auto g-0'><div id='member'><button id='member-butt' onclick='playWindow()'></button></div></div>";
+		var memberID = "member-butt" + addNum;
+		var createDiv = "<div class='col-auto g-0'><div id='member'><button id='"+memberID+"' onclick='playWindow("+addNum+")'></button></div></div>";
 		sumCreate += createDiv;
 		return document.getElementById("members-land").innerHTML = sumCreate;
+		}
 
-		function playWindow() {
-		let playBoard;
-		playBoard = window.open("playBoard.html", "", "width=400,height=900,left=500")
-		} */
-	}
-
-	for (var i=0; i < 504;i++ ) {
+	 for (var i=0; i < 200;i++ ) {
 		counter = i + 1;
 		callCreate(counter);
 	} 
 
 }
+	
+	function playFinish(buttIdNUM) {
+		let IdNUM = buttIdNUM;
+		buttIdNUM = "member-butt"+IdNUM;
+		document.getElementById(buttIdNUM).style = "background-color: pink;";
+		document.getElementById("play-butt").remove();
+	}
 
-function playWindow() {
-	let playBoard;
-	playBoard = window.open("playBoard.html", "", "width=400,height=900,left=500")
-}
+	function playWindow(buttIdNUM) {
+		let IdNUM = buttIdNUM;
+		buttIdNUM = "member-butt"+IdNUM;
+		console.log(buttIdNUM);
+		document.getElementById("play-div").innerHTML = "<button id='play-butt' onclick='playFinish("+IdNUM+")' style='background-color: lightyellow;'>Test</button>";
+	}
 
-function playFinish() {
-	window.close();
-	changeMemberSkin();
-}
 
-function changeMemberSkin() {
-	changeSkin = document.getElementById("member-butt").innerHTML = "<button id='member-butt' onclick='playWindow()' style='background-color: red;'></button>";
-}
+
+
+
+
+/////////////////////////////////////******
+	/******  REFERENCES ********/////
+	/********************************/////
+
+
+	// Test waiting timer //
+	/*const colorWait = async (milliseconds) => {
+	await new Promise(resolve => {
+	    return setTimeout(resolve, milliseconds)
+	});
+	};
+
+	const chColor = async () => {
+    document.getElementById("members-land").innerHTML = "<button id='member-butt' onclick='playWindow()' style='background-color: grey;'>Test</button>"
+	await colorWait(5000);
+    document.getElementById("member-butt").style = "background-color: red;";
+	}
+	chColor();*/
+
+	// Window DOM
+	//window.open("playBoard.html", "", "width=400,height=900,left=500");
+	//window.close();
