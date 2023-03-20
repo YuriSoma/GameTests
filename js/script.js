8f31a97225443b892acfbed84519e4d3f7608f61
YuriSoma <yaser.alelayan@outlook.com>
2023-03-20 20:28:39 +0300

    Refresh Uploading

--- js/script.js
+++ js/script.js
@@ -1,21 +1,25 @@
 function bulidGameLand (){
 	var addNum = 0;
 	var sumCreate = "";
+
+	/*let clanColor = "";
+	document.getElementById("members-land").innerHTML = "<h1>Please choose your CLAN color</h1>";*/
+	
 	function callCreate(addNum) {
 		var memberID = "member-butt" + addNum;
-		var createDiv = "<div class='col-auto g-0'><div id='member'><button id='"+memberID+"' onclick='playWindow("+addNum+")'>"+addNum+"</button></div></div>";
+		var createDiv = "<div class='col-auto g-0'><div id='member'><button id='"+memberID+"' value='"+addNum+"' onclick='playWindow("+addNum+")'>"+addNum+"</button></div></div>";
 		sumCreate += createDiv;
 		return document.getElementById("members-land").innerHTML = sumCreate;
 		}
 
-	 for (var i=0; i < 200;i++ ) {
+	 for (var i=0; i < 250;i++ ) {
 		counter = i + 1;
 		callCreate(counter);
 	} 
 
 }
 	
-	function playFinish(buttIdNUM,windowColor) {
+	/* function playFinish(buttIdNUM,windowColor) {
 		let IdNUM = buttIdNUM;
 		buttIdNUM = "member-butt"+IdNUM;
 		if (windowColor==1){
@@ -26,20 +30,26 @@ function bulidGameLand (){
 		}
 		document.getElementById("play-butt1").remove();
 		document.getElementById("play-butt2").remove();
-	}
+	} */
 
 	function playWindow(buttIdNUM) {
+		
+		///// Makes IDs for every button to be handled later
 		let IdNUM = buttIdNUM;
 		buttIdNUM = "member-butt"+IdNUM;
 		
+		document.getElementById("play-div").focus();
+		document.getElementById("play-div").innerHTML = "<h1 id='challenge'>"+'Challenge On '+IdNUM+"</h1>";
 
 		
+		for (let i = 0; i < 10; i++) {
+			
+		}
+
 		/* document.getElementById("play-div").innerHTML = "<button id='play-butt1' onclick='playFinish("+IdNUM+",1)' style='background-color: lightyellow;'>Test</button><button id='play-butt2' onclick='playFinish("+IdNUM+",2)' style='background-color: pink;'>Test</button>";*/
 		/*document.getElementById("play-div").innerHTML = ;*/
 	}
-
-
-
+	
 
 
 
