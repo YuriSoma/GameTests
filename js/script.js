429da17656cf84c8b00d606a793d706aa478941a
YuriSoma <yaser.alelayan@outlook.com>
2023-04-13 23:07:25 +0300

    Refresh Uploading

--- js/script.js
+++ js/script.js
@@ -7,15 +7,44 @@ function bulidGameLand (){
 	
 	function callCreate(addNum) {
 		var memberID = "member-butt" + addNum;
-		var createDiv = "<div class='col-auto g-0'><div id='member'><button id='"+memberID+"' value='"+addNum+"' onclick='playWindow("+addNum+")'>"+addNum+"</button></div></div>";
+		var createDiv = `<div class='col-auto g-0'
+		><div id='member'><button id='${memberID}' value='${addNum}
+		' onclick='playWindow(${addNum})
+		'>${addNum}</button></div></div>`;
 		sumCreate += createDiv;
 		return document.getElementById("members-land").innerHTML = sumCreate;
 		}
+	
+		const testMD = {};
+		function createObj(counter) {
+		let memberID = "member" + counter;
+		/* let memberDetails = {
+			id:"",
+			memberName:"",
+			health:100,
+			Shield:100,
+			Power:0
+		};*/
+
+		testMD[memberID] = {};
+
+		testMD[memberID].id = counter;
+		testMD[memberID].memberName = counter;
+		testMD[memberID].health = Math.floor(Math.random() * 101);
+		testMD[memberID].Shield = Math.floor(Math.random() * 101);
+		testMD[memberID].Power = Math.floor(Math.random() * 101);
+
+	}
 
 	 for (var i=0; i < 250;i++ ) {
 		counter = i + 1;
 		callCreate(counter);
+		createObj(counter);
 	} 
+
+	console.log(testMD);
+
+	
 
 }
 	
@@ -38,8 +67,34 @@ function bulidGameLand (){
 		let IdNUM = buttIdNUM;
 		buttIdNUM = "member-butt"+IdNUM;
 		
-		document.getElementById("play-div").focus();
-		document.getElementById("play-div").innerHTML = "<h1 id='challenge'>"+'Challenge On '+IdNUM+"</h1>";
+		document.getElementById("play-div").innerHTML = 
+		`<h1 id='challenge'>Challenge On ${IdNUM}</h1>
+		<div class='container text-center
+		'><div class='row' id='play-row'>
+		
+		<div class='col' id='member-gears
+		'><h2 id='mg-title'>Member Gears</h2><button>${IdNUM}
+		</button></div>
+		
+		<div class='col' id='guess-col'><h2 id='gc-title'>Guess...</h2>
+		<form>
+		<label>Health: </label>
+		<input type='number'></input><br>
+		<label>Shield: </label>
+		<input type='number'></input><br>
+		<label>Power: </label>
+		<input type='number'></input><br>
+		<input type='submit'></input>
+		</form>
+		</div>
+		
+		<div class='col' id='member-profile'>
+		<h2 id='mp-title'>Member Profile</h2>
+		</div></div></div>`;
+
+		
+
+		//document.getElementById("play-div").focus();
 
 		
 		for (let i = 0; i < 10; i++) {
